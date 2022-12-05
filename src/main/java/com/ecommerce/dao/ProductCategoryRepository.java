4f3d7baff609c970bf7f5a28f68b8efbc6b0c0fe
Jaime Moises <99954734+Jaimemoises@users.noreply.github.com>
2022-12-05 01:03:17 +0000

    adding rest controller

--- src/main/java/com/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/ecommerce/dao/ProductCategoryRepository.java
@@ -4,8 +4,10 @@ import com.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("http://localhost:4200")
+@RestController
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/ecommerce/dao/StateRepository.java
+++ src/main/java/com/ecommerce/dao/StateRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource
+@RestController
+
 public interface StateRepository extends JpaRepository<State,Integer> {
 
 List<State> findByCountryCode(@Param("code") String code);
