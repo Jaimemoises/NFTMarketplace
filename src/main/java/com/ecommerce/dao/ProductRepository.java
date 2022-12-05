ae50fb23190b90ece6347a52dabba4371370b94c
Jaime Moises <99954734+Jaimemoises@users.noreply.github.com>
2022-12-05 08:50:50 +0000

    To accept external calls

--- src/main/java/com/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/dao/ProductRepository.java
@@ -6,9 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
